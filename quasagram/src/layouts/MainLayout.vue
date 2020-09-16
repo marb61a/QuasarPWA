d226bae3002c8f61947db24244b2fbe7a84d7bd4
Martin <marb61a@gmail.com>
2020-09-16 12:03:40 +0100

    Updated MainLayout with footer and styling

--- quasagram/src/layouts/MainLayout.vue
+++ quasagram/src/layouts/MainLayout.vue
@@ -19,6 +19,13 @@
       </q-toolbar>
     </q-header>
 
+    <q-footer class="bg-white" bordered>
+        <q-tabs class="text-grey-10">
+          <q-route-tab to="/" name='home' icon='mail' label='Home'/>
+          <q-route-tab to="/camera" name='camera' icon='alarm' label='Camera'/>
+        </q-tabs>
+    </q-footer>
+
     <q-page-container>
       <router-view />
     </q-page-container>
