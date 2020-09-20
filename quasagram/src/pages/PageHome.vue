9436318387ce28ad5bff637203be4d4b3e13f522
Martin <marb61a@gmail.com>
2020-09-20 12:11:44 +0100

    updating styling to show mini profile for desktop size screens

--- quasagram/src/pages/PageHome.vue
+++ quasagram/src/pages/PageHome.vue
@@ -1,44 +1,57 @@
 <template>
   <q-page class="constrain q-pa-md">
-    <div>
-      <div>
-        
+    <div class="row q-col-gutter-lg">
+      <div class="col-8">
+        <q-card
+          v-for="post in posts"
+          :key="post.id"
+          class="card-post q-mb-md" 
+          flat 
+          bordered
+        >
+        <q-item>
+          <q-item-section avatar>
+            <q-avatar>
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+            </q-avatar>
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label class="text-bold">Martin_Byrne</q-item-label>
+            <q-item-label caption>
+              {{ post.location }}
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-separator />
+
+        <q-img
+          :src="post.imgUrl"
+        />
+        <q-card-section>
+          <div>{{ post.caption }}</div>
+          <div class="text-caption text-grey">{{ post.date}}</div>
+        </q-card-section>      
+
+      </q-card>
+        </div>
+        <div class="col-4">
+          <q-item>
+            <q-item-section avatar>
+              <q-avatar>
+                <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+              </q-avatar>
+            </q-item-section>
+            <q-item-section>
+              <q-item-label class="text-bold">Martin_Byrne</q-item-label>
+              <q-item-label caption>
+                Martin Byrne
+              </q-item-label>
+            </q-item-section>
+          </q-item>
+        </div>
       </div>
-    </div>
-
-    <q-card
-      v-for="post in posts"
-      :key="post.id"
-      class="card-post q-mb-md" 
-      flat 
-      bordered
-    >
-      <q-item>
-        <q-item-section avatar>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
-          </q-avatar>
-        </q-item-section>
-
-        <q-item-section>
-          <q-item-label class="text-bold">Martin_Byrne</q-item-label>
-          <q-item-label caption>
-            {{ post.location }}
-          </q-item-label>
-        </q-item-section>
-      </q-item>
-
-      <q-separator />
-
-      <q-img
-        :src="post.imgUrl"
-      />
-      <q-card-section>
-        <div>{{ post.caption }}</div>
-        <div class="text-caption text-grey">{{ post.date}}</div>
-      </q-card-section>      
-
-    </q-card>
   </q-page>
 </template>
 
