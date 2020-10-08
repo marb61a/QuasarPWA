f35f6b39a5a1bc8119b092ed5e098803c6238d51
Martin <marb61a@gmail.com>
2020-10-08 11:38:16 +0100

    added firestore dependencies and code for own hosting

--- quasagram/backend/index.js
+++ quasagram/backend/index.js
@@ -1,5 +1,16 @@
 const express = require("express");
 const app = express();
+
+// Connect to the FirebaseDB
+const admin = require("firebase-admin");
+const serviceAccount = require("./accountkey");
+
+admin.initializeApp({
+    credential: admin.credential.cert
+    (serviceAccount)
+});
+const db = admin.firestore();
+
 const port = 3000;
 
 // Middleware
